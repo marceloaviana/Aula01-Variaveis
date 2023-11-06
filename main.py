9bc3df75ba094be77d5659324c50b53f0638148a
marceloaviana <96154514+marceloaviana@users.noreply.github.com>
2023-11-06 05:38:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+# declaração de variáveis
+nome = "Marcelo"
+idade = 43
+altura = 1.82
+peso = 81
+
+print("Olá, mundo!")
+
+# imprimir na tela o valor das variáveis
+print(nome) 
+print(idade)
+print(altura)
+print(peso)
+
+print("O meu nome é", nome, "e tenho", idade, "anos. E tenho,", altura, "m e peso", peso, "kg.")
+
+print(type(nome)) 
+print(type(idade))
+print(type(altura))
+print(type(peso))
+
+aluno = input("Digite o nome do aluno: ")
+print("O aluno é o", aluno)
+
+# Execicio 1
+
+Nome = input("Qual o seu nome: ")
+print("Olá,", Nome, "! Seja bem-vindo ao mundo da programação em Python!")
+
+print(type(Nome))
+
+
+# Exercicio 2
+
+
+seu_nome = input("Digite seu nome: ")
+idade_amigo = input("Digite a idade do seu melhor amigo: ")
+
+mensagem = "Olá, " + seu_nome + "! Seu melhor amigo tem " + idade_amigo + " anos de idade."
+
+print(mensagem)
